a1588c0a2affecc3e477145c10fc78b66b933789
Zakhar Timoshenko <vp1984tanki@gmail.com>
2022-09-29 16:37:28 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:7-jdk11 AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle shadowJar --no-daemon
+
+FROM openjdk:11
+EXPOSE 8080:8080
+RUN mkdir /ridebus
+COPY --from=build /home/gradle/src/build/libs/*.jar /app/ridebus-server.jar
+ENTRYPOINT ["java","-jar","/app/ridebus-server.jar"]
---
+++ src/main/kotlin/by/ridebus/plugins/Routing.kt
@@ -0,0 +1,23 @@
+package by.ridebus.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.http.content.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondRedirect("index.html")
+        }
+        get("/get") {
+            call.respondRedirect("ridebus.db")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/") {
+            resources("static")
+        }
+    }
+}
